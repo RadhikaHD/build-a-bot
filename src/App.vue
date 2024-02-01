eb579d6e1325e6a05cb6008e6105ab83098a91f1
Radhika Dhaipule <Radhika.Dhaipule@cincpro.com>
2024-02-01 10:58:30 -0500

    working robot

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" src="./assets/build-a-bot-logo.png" >
+          Build-a-Bot
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <RobotBuilder/>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    RobotBuilder,
   },
 };
 </script>
 
 <style>
+body{
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
---
+++ src/build/RobotBuilder.vue
@@ -0,0 +1,196 @@
+<template>
+    <div>
+        <div class="top-row">
+          <div class="top part">
+            <img v-bind:src="availableParts.heads[selectedHeadIndex].src" title="head"/>
+            <button v-on:click="selectPreviousHead()" class="prev-selector">&#9668;</button>
+            <button v-on:click="selectNextHead()" class="next-selector">&#9658;</button>
+          </div>
+        </div>
+        <div class="middle-row">
+          <div class="left part">
+            <img v-bind:src="availableParts.arms[selectedLeftArmIndex].src" title="left arm"/>
+            <button v-on:click="selectPreviousLeftArm()" class="prev-selector">&#9650;</button>
+            <button v-on:click="selectNextLeftArm()" class="next-selector">&#9660;</button>
+          </div>
+          <div class="center part">
+            <img v-bind:src="availableParts.torsos[selectedTorsoIndex].src" title="torso"/>
+            <button v-on:click="selectPreviousTorso()" class="prev-selector">&#9668;</button>
+            <button v-on:click="selectNextTorso()" class="next-selector">&#9658;</button>
+          </div>
+          <div class="right part">
+            <img v-bind:src="availableParts.arms[selectedRightArmIndex].src" title="right arm"/>
+            <button v-on:click="selectPreviousRightArm()" class="prev-selector">&#9650;</button>
+            <button v-on:click="selectNextRightArm()" class="next-selector">&#9660;</button>
+          </div>
+        </div>
+        <div class="bottom-row">
+          <div class="bottom part">
+            <img v-bind:src="availableParts.bases[selectedBaseIndex].src" title="base"/>
+            <button v-on:click="selectPreviousBase()" class="prev-selector">&#9668;</button>
+            <button v-on:click="selectNextBase()" class="next-selector">&#9658;</button>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+import availableParts from '../data/parts';
+
+function getPreviousValidIndex(index, length) {
+  const deprecatedIndex = index - 1;
+  return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
+}
+
+function getNextValidIndex(index, length) {
+  const incrementedIndex = index + 1;
+  return incrementedIndex > length - 1 ? 0 : incrementedIndex;
+}
+
+export default {
+  name: 'robotbuilder',
+  data() {
+    return {
+      availableParts,
+      selectedHeadIndex: 0,
+      selectedLeftArmIndex: 0,
+      selectedRightArmIndex: 0,
+      selectedTorsoIndex: 0,
+      selectedBaseIndex: 0,
+    };
+  },
+  methods: {
+    selectNextHead() {
+      this.selectedHeadIndex = getNextValidIndex(this.selectedHeadIndex,
+        this.availableParts.heads.length);
+    },
+    selectPreviousHead() {
+      this.selectedHeadIndex = getPreviousValidIndex(this.selectedHeadIndex,
+        this.availableParts.heads.length);
+    },
+    selectNextLeftArm() {
+      this.selectedLeftArmIndex = getNextValidIndex(this.selectedHeadIndex,
+        this.availableParts.heads.length);
+    },
+    selectPreviousLeftArm() {
+      this.selectedLeftArmIndex = getPreviousValidIndex(this.selectedHeadIndex,
+        this.availableParts.heads.length);
+    },
+    selectNextTorso() {
+      this.selectedTorsoIndex = getNextValidIndex(this.selectedHeadIndex,
+        this.availableParts.heads.length);
+    },
+    selectPreviousTorso() {
+      this.selectedTorsoIndex = getPreviousValidIndex(this.selectedHeadIndex,
+        this.availableParts.heads.length);
+    },
+    selectNextRightArm() {
+      this.selectedRightArmIndex = getNextValidIndex(this.selectedHeadIndex,
+        this.availableParts.heads.length);
+    },
+    selectPreviousRightArm() {
+      this.selectedRightArmIndex = getPreviousValidIndex(this.selectedHeadIndex,
+        this.availableParts.heads.length);
+    },
+    selectNextBase() {
+      this.selectedBaseIndex = getNextValidIndex(this.selectedHeadIndex,
+        this.availableParts.heads.length);
+    },
+    selectPreviousBase() {
+      this.selectedBaseIndex = getPreviousValidIndex(this.selectedHeadIndex,
+        this.availableParts.heads.length);
+    },
+  },
+};
+</script>
+
+<style>
+.part {
+    position: relative;
+    width:165px;
+    height:165px;
+    border: 3px solid #aaa;
+  }
+  .part img {
+    width:165px;
+  }
+  .top-row {
+    display:flex;
+    justify-content: space-around;
+  }
+  .middle-row {
+    display:flex;
+    justify-content: center;
+  }
+  .bottom-row {
+    display:flex;
+    justify-content: space-around;
+    border-top: none;
+  }
+  .head {
+    border-bottom: none;
+  }
+  .left {
+    border-right: none;
+  }
+  .right {
+    border-left: none;
+  }
+  .left img {
+    transform: rotate(-90deg);
+  }
+  .right img {
+    transform: rotate(90deg);
+  }
+  .bottom {
+    border-top: none;
+  }
+  .prev-selector {
+    position: absolute;
+    z-index:1;
+    top: -3px;
+    left: -28px;
+    width: 25px;
+    height: 171px;
+  }
+  .next-selector {
+    position: absolute;
+    z-index:1;
+    top: -3px;
+    right: -28px;
+    width: 25px;
+    height: 171px;
+  }
+  .center .prev-selector, .center .next-selector {
+    opacity:0.8;
+  }
+  .left .prev-selector {
+    top: -28px;
+    left: -3px;
+    width: 144px;
+    height: 25px;
+  }
+  .left .next-selector {
+    top: auto;
+    bottom: -28px;
+    left: -3px;
+    width: 144px;
+    height: 25px;
+  }
+  .right .prev-selector {
+    top: -28px;
+    left: 24px;
+    width: 144px;
+    height: 25px;
+  }
+  .right .next-selector {
+    top: auto;
+    bottom: -28px;
+    left: 24px;
+    width: 144px;
+    height: 25px;
+  }
+  .right .next-selector {
+    right: -3px;
+  }
+</style>
